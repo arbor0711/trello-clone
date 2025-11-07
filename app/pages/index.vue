eb6a11eabb57a436a63293af1ebf72cce9b3b2ae
Alireza <akeshavarz@harlemnext.com>
2025-11-07 11:36:36 +0100

    install and configure project with nuxt, pinia, and nuxt ui

---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { useBoardStore } from "../../stores/boardStore"
+
+const boardStore = useBoardStore();
+console.log(boardStore.board)
+</script>
+
+<template>
+    <main>
+        <h1>Hello from Nuxt3</h1>
+        <pre>{{ boardStore.board }}</pre>
+    </main>
+</template>
