a916d322c45a3010d2d9b468acca92125412e121
Alireza <akeshavarz@harlemnext.com>
2025-11-08 08:54:24 +0100

    build UI

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,13 +1,26 @@
 <script setup>
-import { useBoardStore } from "../../stores/boardStore"
+import { useBoardStore } from "../../stores/boardStore";
 
 const boardStore = useBoardStore();
-console.log(boardStore.board)
+const { board } = boardStore;
 </script>
 
 <template>
-    <main>
-        <h1>Hello from Nuxt3</h1>
-        <pre>{{ boardStore.board }}</pre>
-    </main>
+  <main class="h-screen w-screen bg-emerald-500 p-4">
+    <UContainer class="flex flex-row items-start gap-4 bg-emerald-500">
+      <ul
+        v-for="column in board.columns"
+        key="column.name"
+        class="bg-gray-50 px-4 pt-4 rounded-lg h-max flex-1"
+      >
+        <h2 class="font-bold bg-gray-200 p-4 rounded-lg mb-4">{{ column.name.toUpperCase() }}</h2>
+        <li>
+          <UCard v-for="task in column.tasks" key="task.id" class="mb-4">
+            <strong>{{ task.name }}</strong>
+            <p>{{ task.description }}</p>
+          </UCard>
+        </li>
+      </ul>
+    </UContainer>
+  </main>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+
   modules: ['@nuxt/ui', '@pinia/nuxt'],
-  css: ['~/assets/css/main.css'],
+  css: [ '~/assets/css/main.css'],
   colorMode: {
-    preference: "light"
+    preference: 'light'
   }
 })
